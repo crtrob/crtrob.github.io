70195af6b0c2949a32e9017e8740d1b2f82e8ba5
crtrob <carterrobertsprf@gmail.com>
2021-10-09 16:57:36 +0000

    add platforms and collectables to ame

--- projects/platformer/js/init/collectable.js
+++ projects/platformer/js/init/collectable.js
@@ -12,37 +12,11 @@
         kennedi: {assetKey: 'kennedi', points: 50}
     };
     
-    /**
-     * init: Initialize all collectables.
-     * 
-     * GOAL: Add as many collectables as necessary to make your level challenging.
-     * 
-     * Use the createCollectable() Function to create collectables for the level.
-     * See the type Object, above, for the types of collectables and their point values.
-     * 
-     * createCollectable() takes these arguments:
-     *      
-     *      createCollectable(type, x, y, gravity, bounce);
-     * 
-     *      type: The type of the collectable, use the type Object above.
-     *      x: The x coordineate for the collectable.
-     *      y: The y coordineate for the collectable.
-     *      gravity: OPTIONAL The gravitational pull on the collectable.
-     *      bounce: OPTIONAL A factor effecting how much the collectable will bounce off platforms, etc.
-     */ 
     function init(game) {
         let createCollectable = collectable.create;
-
-        ////////////////////////////////////////////////////////////////////////
-        // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
-        
-        // example: 
-        createCollectable(type.steve, 200, 170, 6, 0.7);
-        
-        
-        
-        // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
-        ////////////////////////////////////////////////////////////////////////
+            createCollectable(type.max, 625, 600,);
+            createCollectable(type.grace, 490, 400);
+            createCollectable(type.kennedi, 450, 250);
     };
     collectable.init = init;
 })(window);
